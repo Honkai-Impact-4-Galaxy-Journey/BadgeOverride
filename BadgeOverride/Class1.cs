9c7a931a9a129b60f18a0982560934c89a084959
Tiphereth <atmlz32@163.com>
2023-12-21 19:37:37 +0800

    添加项目文件。

---
+++ BadgeOverride/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadgeOverride
+{
+    public class Class1
+    {
+    }
+}
